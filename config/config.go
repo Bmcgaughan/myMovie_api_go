ac373e2d5a4679fa3d96f620a9f0a9ffc989b30b
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-12-30 21:26:55 -0500

    updates to config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,9 @@
 package config
 
 import (
-	"log"
+	"os"
 
 	"github.com/go-redis/redis/v8"
-	viper "github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -18,29 +17,40 @@ type APIConfig struct {
 	APIKey        string `mapstructure:"API_KEY"`
 	JWTSecret     string `mapstructure:"JWT_SECRET"`
 	ImageBase     string `mapstructure:"IMAGE_BASE"`
-	Cost          int    `mapstructure:"COST"`
 	AllowedOrigin string `mapstructure:"ALLOWED_ORIGINS"`
 }
 
 var MainConfig APIConfig
 
 func LoadConfig() {
-	// load .env file from root if exists
-	v := viper.New()
-	v.BindEnv("PORT")
-	v.BindEnv("MONGODB_URI")
-	v.BindEnv("REDIS_URI")
-	v.BindEnv("REDIS_PASSWORD")
-	v.BindEnv("API_KEY")
-	v.BindEnv("JWT_SECRET")
-	v.BindEnv("IMAGE_BASE")
-	v.BindEnv("COST")
-	v.BindEnv("ALLOWED_ORIGINS")
 
-	//load viper into Config struct
-	err := v.Unmarshal(&MainConfig)
-	if err != nil {
-		log.Fatalf("unable to decode into struct, %v", err)
+	MainConfig = APIConfig{
+		Port:          os.Getenv("PORT"),
+		MongoDBURI:    os.Getenv("MONGODB_URI"),
+		RedisURI:      os.Getenv("REDIS_URI"),
+		RedisPassword: os.Getenv("REDIS_PASSWORD"),
+		APIKey:        os.Getenv("API_KEY"),
+		JWTSecret:     os.Getenv("JWT_SECRET"),
+		ImageBase:     os.Getenv("IMAGE_BASE"),
+		AllowedOrigin: os.Getenv("ALLOWED_ORIGINS"),
 	}
+
+	// load .env file from root if exists
+	// v := viper.New()
+	// v.BindEnv("PORT")
+	// v.BindEnv("MONGODB_URI")
+	// v.BindEnv("REDIS_URI")
+	// v.BindEnv("REDIS_PASSWORD")
+	// v.BindEnv("API_KEY")
+	// v.BindEnv("JWT_SECRET")
+	// v.BindEnv("IMAGE_BASE")
+	// v.BindEnv("COST")
+	// v.BindEnv("ALLOWED_ORIGINS")
+
+	// //load viper into Config struct
+	// err := v.Unmarshal(&MainConfig)
+	// if err != nil {
+	// 	log.Fatalf("unable to decode into struct, %v", err)
+	// }
 
 }
